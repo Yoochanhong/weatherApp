c694b90af281e7fc2e18969103931b8a88e3aa72
Yoochanhong <chanhongy6@naver.com>
2022-09-03 20:53:29 +0900

    날씨 앱 만들기

--- weather_app/lib/screens/loading.dart
+++ weather_app/lib/screens/loading.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert'; //jsonDecode를 사용할 수 있게 해줌
 
 class Loading extends StatefulWidget {
   const Loading({Key? key}) : super(key: key);
@@ -11,11 +13,31 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
+  @override
+  void initState(){
+    super.initState();
+    getLocation();
+    feachData();
+  }
+
   void getLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();//위치정보 동의
-    Position position = await Geolocator.
-    getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-    print(position);
+    try {
+      Position position = await Geolocator.
+      getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    }catch(e){
+      print('인터넷 연결에 문제가 생겼습니다.');
+    }
+  }
+
+  void feachData() async{
+    http.Response response = await http.get(Uri.parse('https://samples.openweathermap.org/data/2.5/weather?'
+        'q=London&appid=b1b15e88fa797225412429c1c50c122a1'));
+    if(response.statusCode == 200){
+      String jsonData = response.body;
+      var myJson = jsonDecode(jsonData)['weather'][0]['description'];
+      print(myJson);
+    }
   }
 
   @override
@@ -24,7 +46,6 @@ class _LoadingState extends State<Loading> {
       body: Center(
         child: CupertinoButton(
           onPressed: (){
-            getLocation();
           },
           child: Text(
             '내 위치',
